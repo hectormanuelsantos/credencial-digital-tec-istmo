7f956dc9d65834918157921ca99f5b0fc18db514
Hector Manuel Santos <hectormanuelsantosbautista27@gmail.com>
2022-02-02 12:12:07 -0600

    updated conditional to get vigency

--- src/helpers/GetVigencyCredential.js
+++ src/helpers/GetVigencyCredential.js
@@ -1,8 +1,7 @@
 const date = new Date();
 let month = date.getMonth();
 let year = date.getFullYear();
-let vigency;
 
-month >= 0 && month <= 6 ? (vigency = `ENE - JUL ${year}`) : (vigency = `AGO - DIC ${year}`);
+let vigency = month >= 0 && month <= 6 ? `ENE - JUL ${year}` : `AGO - DIC ${year}`;
 
 export { vigency };
