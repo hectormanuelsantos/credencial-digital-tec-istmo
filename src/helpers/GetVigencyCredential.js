deed98a7bb399f9a03f46b619e481819464d2856
Hector Manuel Santos <hectormanuelsantosbautista27@gmail.com>
2022-02-02 09:02:08 -0600

    update GetVigencyCredential.js

--- src/helpers/GetVigencyCredential.js
+++ src/helpers/GetVigencyCredential.js
@@ -3,6 +3,6 @@ let month = date.getMonth();
 let year = date.getFullYear();
 let vigency;
 
-(month >= 0 && month <= 6) ? vigency = `ENE - JUL ${year}` : vigency = `AGO - DIC ${year}`
+month >= 0 && month <= 6 ? (vigency = `ENE - JUL ${year}`) : (vigency = `AGO - DIC ${year}`);
 
 export { vigency };
