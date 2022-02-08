83d6b46ee75d8d714a04a7f4967a3d593854dac0
Hector Manuel Santos <hectormanuelsantosbautista27@gmail.com>
2022-02-08 15:02:36 -0600

    Remove font LatoLight

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import MainStack from './src/navigation/MainStack';
 
 const App = () => {
   const [loaded] = useFonts({
-    LatoLight: require('./src/assets/fonts/LatoLight.ttf'),
     LatoRegular: require('./src/assets/fonts/LatoRegular.ttf'),
     LatoBold: require('./src/assets/fonts/LatoBold.ttf'),
     LatoBlack: require('./src/assets/fonts/LatoBlack.ttf'),
