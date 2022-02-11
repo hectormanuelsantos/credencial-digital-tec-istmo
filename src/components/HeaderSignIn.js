c2727e5d6805c2ee617fad68a91dc9b0011aa3c8
Hector Manuel Santos <hectormanuelsantosbautista27@gmail.com>
2022-02-10 18:25:32 -0600

    Update HeaderSignIn.js

--- src/components/HeaderSignIn.js
+++ src/components/HeaderSignIn.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Image, StyleSheet, View } from 'react-native';
-import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const HeaderSignIn = () => {
   return (
